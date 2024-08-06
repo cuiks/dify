c1e09516732dc04a3331a84bd7dff2f946a7628b
cuiks <1253016652@qq.com>
2024-08-06 11:47:43 +0800

    feat: img2img

--- api/core/tools/provider/builtin/hortorcomfyui/tools/comfyui.py
+++ api/core/tools/provider/builtin/hortorcomfyui/tools/comfyui.py
@@ -3,7 +3,7 @@ from typing import Any, Union
 
 import requests
 
-from core.tools.entities.tool_entities import ToolInvokeMessage
+from core.tools.entities.tool_entities import ToolInvokeMessage, ToolParameter
 from core.tools.tool.builtin_tool import BuiltinTool
 
 
@@ -26,11 +26,32 @@ class ComfyuiTool(BuiltinTool):
             return self.create_text_message('Please input prompt')
         negative_prompt = tool_parameters.get('negative_prompt', '')
 
+        img_url = ""
+        if model in ["sqxly_img2img"]:
+            image_id = tool_parameters.get('image_id', '')
+            if not image_id:
+                return self.create_text_message('Please input image id')
+
+            image_binary = self.get_variable_file(self.VARIABLE_KEY.IMAGE)
+            if not image_binary:
+                return self.create_text_message('Image not found, please request user to generate image firstly.')
+
+            # 二进制image_binary, 上传到 http://background.hortorgames.com/background/v1/file/upload
+            response = requests.post('https://background.hortorgames.com/background/v1/file/upload', files={
+                'file': image_binary
+            })
+            if response.status_code != 200:
+                raise Exception('Request failed')
+            if response.json().get("meta", {}).get("errCode", -1) != 0:
+                raise Exception(response.json().get("meta", {}).get("errMsg", 'Request failed'))
+            img_url = response.json().get("data", "")
+
         response = requests.post(base_url, json={
             "version": model,
             "param": {
                 "prompt": prompt,
-                "negative_prompt": negative_prompt
+                "negative_prompt": negative_prompt,
+                "image": img_url
             }
         })
         if response.status_code != 200:
@@ -39,7 +60,6 @@ class ComfyuiTool(BuiltinTool):
             raise Exception(response.json().get("meta", {}).get("errMsg", 'Request failed'))
 
         result = []
-
         for image in response.json().get("data", {}).get("list", []):
             result.append(self.create_image_message(image=image))
 
@@ -50,3 +70,19 @@ class ComfyuiTool(BuiltinTool):
         characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
         random_id = ''.join(random.choices(characters, k=length))
         return random_id
+
+    def get_runtime_parameters(self) -> list[ToolParameter]:
+        """
+        override the runtime parameters
+        """
+        return [
+            ToolParameter.get_simple_instance(
+                name='image_id',
+                llm_description=f'the image id that you want to vectorize, \
+                    and the image id should be specified in \
+                        {[i.name for i in self.list_default_image_variables()]}',
+                type=ToolParameter.ToolParameterType.SELECT,
+                required=True,
+                options=[i.name for i in self.list_default_image_variables()]
+            )
+        ]
